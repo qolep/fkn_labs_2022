6f714ce08c5080c172348508e774dbfaadbe720d
s_nikitaf1 <nikitaf1@yandex.ru>
2022-10-21 03:52:02 +0300

    lab1
    without color switch on a background triangle. Might fix it soon

---
+++ app/src/main/java/com/example/effective_lab/Hero.kt
@@ -0,0 +1,7 @@
+package com.example.effective_lab
+
+data class Hero(
+    val image: Int,
+    val name: String,
+    val description: String,
+)
---
+++ app/src/main/java/com/example/effective_lab/HeroList.kt
@@ -0,0 +1,40 @@
+package com.example.effective_lab
+
+object HeroList {
+    val heroes = listOf(
+        Hero(
+            R.drawable.blade,
+            "Blade",
+            ""
+
+        ),
+        Hero(
+            R.drawable.quicksilver,
+            "Quicksilver",
+            ""
+
+        ),
+        Hero(
+            R.drawable.blackpanther,
+            "Black Panther",
+            ""
+
+        ),
+
+        Hero(
+            R.drawable.blackcat,
+            "Black cat",
+            ""
+
+        ),
+
+
+        Hero(
+            R.drawable.thor,
+            "Thor",
+            ""
+
+        ),
+    )
+
+}
